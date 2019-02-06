5adbdb605843f197a28c6aec3d1ea8d347864420
Mohamad Zain Aziz <zain@zymplsi.com>
2019-02-06 12:54:31 +0800

    remove student and teacher apis

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,9 @@
-import { teacherRouter } from './teacher.routes';
-import { studentRouter } from './student.routes';
 import { registerRouter } from './register.routes';
 import { commonStudentsRouter } from './commonstudents.routes';
 import { retrieveForNotificationsRouter } from './retrievefornotifications.routes';
 import { suspendRouter } from './suspend.routes';
 
 export const apiRoutes = [
-  ...teacherRouter,
-  ...studentRouter,
   ...registerRouter,
   ...commonStudentsRouter,
   ...retrieveForNotificationsRouter,
