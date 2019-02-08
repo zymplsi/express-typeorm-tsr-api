14aae90c477fd25875fffb029fde4597291e4d97
Mohamad Zain Aziz <zain@zymplsi.com>
2019-02-08 18:09:13 +0800

    refactor unit test

--- test/unit/controllers/suspend.controller.test.ts
+++ test/unit/controllers/suspend.controller.test.ts
@@ -1,25 +1,17 @@
 import 'source-map-support/register';
 import chai, { expect } from 'chai';
 import * as typeorm from 'typeorm';
-import { stub, createStubInstance, spy } from 'sinon';
+import { stub, createStubInstance, restore } from 'sinon';
 import { SuspendController } from '../../../src/controller/suspend.controller';
 import { Registration } from '../../..//src/entity/Registration';
-import { Request, Response, NextFunction } from 'express';
-import { mockRes } from 'sinon-express-mock';
 import sinonChai from 'sinon-chai';
 
 chai.use(sinonChai);
 
 describe('SuspendController', async () => {
-  let req: Partial<Request>;
-  let res: Response;
-  let next: NextFunction;
-
-  beforeEach(() => {
-    res = mockRes();
-    next = stub();
+  after(() => {
+    restore();
   });
-
   it('should call the Querybuilder', async () => {
     const fakeRepository: typeorm.Repository<Registration> = createStubInstance(
       typeorm.Repository
@@ -30,42 +22,19 @@ describe('SuspendController', async () => {
     expect(fakeRepository.createQueryBuilder).calledWith('registration');
   });
 
-  it('should validate student email', async () => {
-    req = {
-      body: {
-        student: 'abc@123.com'
-      }
-    };
+  it('should validate emails', async () => {
+    const correctEmail = 'abc@abc.com';
+    const wrongEmail = 'abc.abc.com';
     const suspendController = new SuspendController();
-    const spyValidateStudent = spy(suspendController, 'validateStudent');
-    await suspendController.update(<any>req, <any>res, next);
 
-    expect(spyValidateStudent).to.be.calledWith(req.body.student);
-  });
-
-  it("should throw 400 'email is not valid', when student email is empty", async () => {
-    req = {
-      body: {
-        student: ''
-      }
-    };
-
-    const suspendController = new SuspendController();
-    await suspendController.update(<any>req, <any>res, next);
-    expect(res.status).to.be.calledWith(400);
-    expect(res.send).to.be.calledWith('email is not valid');
-  });
-
-  it("should throw 400 'email is not valid', when student email is invalid", async () => {
-    req = {
-      body: {
-        student: 'abc123'
-      }
-    };
-
-    const suspendController = new SuspendController();
-    await suspendController.update(<any>req, <any>res, next);
-    expect(res.status).to.be.calledWith(400);
-    expect(res.send).to.be.calledWith('email is not valid');
+    suspendController.validateStudent(correctEmail).catch(error => {
+      /** error is thrown from Typeorm query which is after the validate function */
+      expect(error.message).to.be.equal(
+        "Cannot read property 'where' of undefined"
+      );
+    });
+    suspendController.validateStudent(wrongEmail).catch(error => {
+      expect(error.message).to.be.equal('email is not valid');
+    });
   });
 });
